9ed2fdf7879ea8350f8c0ffcce4d7008798c73cb
tumoulin <dylan.ryder@my.jcu.edu.au>
2024-10-21 13:29:23 +1000

    Prac 5 emails exercise complete

---
+++ prac_05/emails.py
@@ -0,0 +1,30 @@
+"""
+Emails
+Estimate: 25 minutes
+Actual: 19 minutes
+"""
+
+
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is this your name: {name}? (Y/N) ").upper()
+        if confirmation != "Y" and confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def get_name_from_email(email):
+    prefix = email.split("@")[0]
+    parts = prefix.split(".")
+    name = " ".join(parts).title()
+    return name
+
+
+main()
